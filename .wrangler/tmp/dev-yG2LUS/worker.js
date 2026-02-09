d69925797728d2584e10c3e02ae6026793dafc2c
JerCol <jeroen.colon@gmail.com>
2026-02-09 17:19:48 +0100

    prevent key leakage

--- .wrangler/tmp/dev-yG2LUS/worker.js
+++ .wrangler/tmp/dev-yG2LUS/worker.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-oHJzkc/checked-fetch.js
+// .wrangler/tmp/bundle-x1fvA6/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -250,7 +250,7 @@ var drainBody = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "drainBody");
 var middleware_ensure_req_body_drained_default = drainBody;
 
-// .wrangler/tmp/bundle-oHJzkc/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-x1fvA6/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default
 ];
@@ -281,7 +281,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-oHJzkc/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-x1fvA6/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
